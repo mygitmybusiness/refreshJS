db1c4294432ccc26cde0d17a509d42dd8317ef8b
dDein <dropsy.dice@gmail.com>
2026-05-14 17:58:00 +0200

    Promise race

---
+++ promiseRace.js
@@ -0,0 +1,19 @@
+function promiseRace(promises) {
+  if (!promises.length) return Promise.reject("input array is empty");
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((element) => {
+      element.then((val) => resolve(val)).catch((err) => reject(err));
+    });
+  });
+}
+
+const result = await promiseRace([
+  new Promise((res) => setTimeout(() => res("slow"), 4000)),
+  new Promise((res) => setTimeout(() => res("fast"), 3000)),
+  new Promise((res) => setTimeout(() => res("slowest"), 5000)),
+]);
+
+// result = 'fast'
+
+console.log(result);
