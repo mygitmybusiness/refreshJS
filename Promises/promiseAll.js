d94c53ea1a2e5b41d0580957ec9dd7448bd45ebc
dDein <dropsy.dice@gmail.com>
2026-06-14 14:26:26 +0200

    fix promiseAll

--- Promises/promiseAll.js
+++ Promises/promiseAll.js
@@ -1,22 +1,37 @@
-async function promiseAll(promises) {
-    let results = Array.from({length: promises.length});
+function promiseAll(promises) {
+  return new Promise((resolve, reject) => {
+    if (!promises.length) {
+      resolve([]);
+    }
+
+    let results = Array.from({ length: promises.length });
+    let counter = promises.length;
 
     promises.forEach((promise, index) => {
-        Promise.resolve(promise).then((data) => {
-            results[index] = data;
-        }).catch((err) => {
-            throw new Error(err);
+      Promise.resolve(promise)
+        .then((data) => {
+          results[index] = data;
+          counter--;
+
+          if (counter == 0) {
+            resolve(results);
+          }
         })
-    })
+        .catch(reject);
+    });
+  });
+}
 
-    return results;
-  }
+const check = async () => {
+    const promises = await promiseAll([
+        Promise.resolve(1),
+        new Promise((res, rej) => setTimeout(() => res("SLOW"), 2000)),
+        Promise.resolve(3),
+        Promise.resolve(4),
+    ]);
 
-  const results = await promiseAll([
-    Promise.resolve(1),
-    Promise.resolve(2),
-    Promise.resolve(3),
-    Promise.resolve(4)
-  ]);
+    console.log(promises);
+}
 
-  console.log(results);
+check();
+
